c3dc01ef18c3757a1a163828fd3b75674fa22de0
freitaseric <contato@freitaseric.com>
2026-02-13 22:14:01 -0400

    feat 2

--- shared/src/commonMain/kotlin/com/freitaseric/siga/data/database/DriverFactory.kt
+++ shared/src/commonMain/kotlin/com/freitaseric/siga/data/database/DriverFactory.kt
@@ -1,7 +1,9 @@
 package com.freitaseric.siga.data.database
 
 import app.cash.sqldelight.db.SqlDriver
+import com.freitaseric.siga.data.repository.ReportRepository
 import com.freitaseric.siga.db.AppDatabase
+import com.freitaseric.siga.db.ServiceEntity
 
 expect class DriverFactory {
     fun createDriver(): SqlDriver
@@ -9,5 +11,5 @@ expect class DriverFactory {
 
 fun createDatabase(driverFactory: DriverFactory): AppDatabase {
     val driver = driverFactory.createDriver()
-    return AppDatabase(driver)
+    return AppDatabase(driver, ServiceEntity.Adapter(categoryAdapter = ReportRepository.serviceCategoryAdapter))
 }
